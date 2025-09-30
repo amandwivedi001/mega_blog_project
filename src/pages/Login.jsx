7d7f4f91660bc514735b5321237c8b40f84ad500
amandwivedi001 <amandwivedi[D262@gmail.com>
2025-09-30 21:57:57 +0530

    first commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Login as LoginComponent } from '../Components'
+
+function Login() {
+    return (
+        <LoginComponent />
+    )
+}
+
+export default Login
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Signup as SignupComponent } from '../Components'
+
+function SignUp() {
+    return (
+        <SignupComponent />
+    )
+}
+
+export default SignUp
